d82cf616f6df732c1de32c239e0e31fca61ae232
Sebastian Balzer <sebastian.balzer@gmx.ch>
2023-04-13 08:26:02 +0200

    config init

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import os
+import hydra
 
+import pandas as pd
 from loguru import logger
+from omegaconf import DictConfig, OmegaConf
 
-
-def main() -> None:
-    pass
+@hydra.main(version_base=None, config_path='.', config_name='config')
+def main(config: DictConfig) -> None:
+    file_path = config.file_path
+    data = pd.read_excel(file_path)
+    
 
 
 if __name__ == '__main__':
