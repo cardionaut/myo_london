d8fb117265b02e60a49647cea10cba3a9cb8bbff
Sebastian Balzer <sebastian.balzer@gmx.ch>
2023-04-17 10:33:46 +0200

    added excelwriter to set datetime format

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def main(config: DictConfig) -> None:
 
     out_dir = os.path.dirname(file_path)
     new_file_name = f'{Path(file_path).stem}_cleaned.xlsx'
-    new_data.to_excel(os.path.join(out_dir, new_file_name), index=False)
+    with pd.ExcelWriter(os.path.join(out_dir, new_file_name), datetime_format='DD/MM/YYYY') as writer:
+        new_data.to_excel(writer, index=False)
 
 if __name__ == '__main__':
     main()
