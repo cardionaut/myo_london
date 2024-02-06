1a545e467fab742f0bf950b06243e9ee62911229
Sebastian Balzer <sebastian.balzer@gmx.ch>
2024-02-06 07:44:04 +0100

    myo_london checkpoint

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 from utils.cleaner import Cleaner
 
+
 @hydra.main(version_base=None, config_path='.', config_name='config')
 def main(config: DictConfig) -> None:
     file_path = config.file_path
@@ -20,6 +21,7 @@ def main(config: DictConfig) -> None:
     new_file_name = f'{Path(file_path).stem}_cleaned.xlsx'
     with pd.ExcelWriter(os.path.join(out_dir, new_file_name), datetime_format='DD/MM/YYYY') as writer:
         new_data.to_excel(writer, index=False)
+
 
 if __name__ == '__main__':
     main()
